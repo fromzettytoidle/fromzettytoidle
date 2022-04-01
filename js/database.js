aaf981c7d3c1bbb0a572b83d56657438c5beec35
zettyaffandi <zettyaffandi@gmail.com>
2022-04-02 06:35:03 +0800

    Add all files

---
+++ js/database.js
@@ -0,0 +1,13 @@
+//
+// const commentForm = document.getElementById('contact');
+//
+// commentForm.addEventListener('submit', (e) =>{
+//   e.preventDefault();
+//   db.collection("minnie").add({
+//        name: commentForm.name.value,
+//        comments: commentForm.comment.value,
+//      });
+//      commentForm.name.value ='';
+//      commentForm.comment.value = ''}
+//
+// )
